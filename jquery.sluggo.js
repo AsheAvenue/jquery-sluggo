9ff3d0a6b1e1482acd7a01bbe95335df0f49b573
Joe Komenda <jkomenda@asheavenue.com>
2013-07-03 15:36:45 -0400

    Space/tab cleanup, additional documentation in top of uncompressed plugin, and updated example to reflect the default inputs.

--- jquery.sluggo.js
+++ jquery.sluggo.js
@@ -1,17 +1,20 @@
-// Sluggo, a jQuery plugin, version 1.0, by komejo (asheavenue.com)
+// Sluggo, a jQuery plugin, version 1.1, by komejo and Tim Boisvert (asheavenue.com).
 //
 // Simplest usage: $('.source').sluggo('.target');
-// Result: converts to lowercase and replaces any non-aplhanumeric with an underscore.
+// Result: converts to lowercase and replaces any non-aplhanumeric with an dash.
+// Multiple spaces are collapsed, and leading and trailing spaces are trimmed.
+// Need to be used with form elements, as it uses the 'val' method.
 //
 // Options: (target: is required)
 //
 // $('.source').sluggo({
-// 	target:  	"",     // Any valid jQuery selector, e.g.: ".target".
-// 	spacer:  	"_",    // Default is underscore. Set to "" for none.
-// 	prefix:  	"",		// None by default.
-// 	suffix:  	"",		// None by default.
-// 	ext:     	"",     // File extention, none by default. Include a spacer if desired, e.g.: ".txt".
-// 	date: 		false   // False is default, true is unspaced YYYMMDD. Anything in quotes, e.g. "-" is true AND the date-only spacer.
+//  target:             "",     // Any valid jQuery selector, e.g.: ".target".
+//  spacer:             "-",    // Default is underscore. Set to "" for none.
+//  prefix:             "",     // None by default.
+//  suffix:             "",     // None by default.
+//  ext:                "",     // File extention, none by default. Include a spacer if desired, e.g.: ".txt".
+//  date:               false   // False is default, true is unspaced YYYMMDD. Anything in quotes, e.g. "-" is true AND the date-only spacer.
+//  jQueryFillMethod:   "val"   // Default is for input->input use. Switch to 'text' for input->div (or other non-editable fields).
 // });
 
 (function ( $ ) {
@@ -28,13 +31,13 @@
 		}
 
 		function reString( value ) {
-            
-            // Drop the source text to lowercase and trim it (in order to prevent a slug 
-            // from starting or ending with the spacer character)
+
+			// Drop the source text to lowercase and trim it (in order to prevent a slug
+			// from starting or ending with the spacer character)
 			value = value.toLowerCase().trim();
-            
-            // Replace all multi-space sections with a single space
-            value = value.replace(/ +(?= )/g,''); 
+
+			// Replace all multi-space sections with a single space
+			value = value.replace(/ +(?= )/g,'');
 
 			// General Replacement - anything that isn't an alphnumeric
 			value = value.replace(/[^a-z0-9]/g, opts.spacer);
@@ -55,24 +58,24 @@
 			// prefix, suffix, date, extension
 			value = opts.prefix + value + opts.suffix + strDate + opts.ext;
 
-	        return value;
+			return value;
 		}
 
 		// detect any change to the source, parse it, insert it into the target (replaces any existing content).
-    	this.keyup(function(e){
-   			$(target)[opts.jQueryFillMethod](reString(e.target.value));
+		this.keyup(function(e){
+			$(target)[opts.jQueryFillMethod](reString(e.target.value));
 		});
-	    return this;
+		return this;
 	};
 
 	// defaults, edit here at your peril.
 	$.fn.sluggo.defaults = {
-		target:  	        "",
-		spacer: 	        "-",
-		prefix:  	        "",
-		suffix:  	        "",
-		ext:     	        "",
-		date: 		        false,
+		target:             "",
+		spacer:             "-",
+		prefix:             "",
+		suffix:             "",
+		ext:                "",
+		date:               false,
 		jQueryFillMethod:   "val"
 	};
 
